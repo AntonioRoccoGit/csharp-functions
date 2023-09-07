2edf9c40406145ef18c55f303cfb4deb09c7a576
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-09-07 15:23:20 +0200

    added bonus task

--- csharp-functions/Program.cs
+++ csharp-functions/Program.cs
@@ -2,95 +2,105 @@
 {
     internal class Program
     {
+        ////////////////////////////////////////////////
+        ////////////////////PROGRAM/////////////////////
+        ////////////////////////////////////////////////
         static void Main(string[] args)
         {
-
 
             /*
                 BONUS:
                 Convertire le funzioni appena dichiarate in funzioni generiche, ossia funzioni che possono lavorare con array di numeri interi di lunghezza variabile, ossia debbono poter funzionare sia che gli passi array di 5 elementi, sia di 6, di 7, di ... e così via. A questo punto modificare il programma in modo che chieda all’utente quanti numeri voglia inserire, e dopo di che questi vengono inseriti a mano dall’utente esternamente.
                 Rieseguire il programma con l’input preso esternamente dall’utente. 
              */
+            
+            Console.WriteLine("Quanti numeri vuoi digitare?");
+            int arrayLength = int.Parse(Console.ReadLine());
+            int[] userArray = new int[arrayLength];
 
-            ////////////////////PROGRAM/////////////////////
-            int[] userArray = { 2, 6, 7, 5, 3, 9, 12, 6, 7, 11};
+            for (int i = 0; i < arrayLength; i++)
+            {
+                Console.Write($"Inserire {i+1}° numero: ");
+                userArray[i] = int.Parse(Console.ReadLine());
+            }
+
 
             Console.Write("Array originale: ");
-            printArray(userArray);
+            PrintArray(userArray);
 
             Console.WriteLine();
             Console.Write("Array al quadrato: ");
-            printArray(arrayToSquare(userArray));
+            PrintArray(ArrayToSquare(userArray));
 
             Console.WriteLine();
             Console.Write("Somma elementi array: ");
-            Console.Write(arrayElementSum(userArray));
+            Console.Write(ArrayElementSum(userArray));
 
             Console.WriteLine();
             Console.Write("Somma elementi array elevati al quadrato: ");
-            Console.Write(arrayElementSum(arrayToSquare(userArray)));
+            Console.Write(ArrayElementSum(ArrayToSquare(userArray)));
 
             Console.WriteLine();
             Console.Write("Array originale: ");
-            printArray(userArray);
-
-            
-
-            
-
-            /////////////////////FUNCTION///////////////////
-
-            //print an array in that format [element1, element2, element3, ...]
-            void printArray(int[] array)
-            {
-                Console.Write("[");
-
-                for (int i = 0; i < array.Length; i++) { 
-                    int currentNumber = array[i];
-                    if (i < array.Length -1) { 
-                        Console.Write($"{currentNumber}, ");
-                    }else
-                    {
-                        Console.Write(currentNumber);
-                    } 
-
-                }
-
-                Console.Write("]");
-                
-            }
-            
-            //int sommaElementiArray(int[] array): che preso un array di numeri interi, restituisca la somma totale di tutti gli elementi dell’array.
-            int arrayElementSum(int[] array)
-            {
-                int sum = 0;
-                for (int i = 0; i < array.Length; i++)
-                {
-                    sum += array[i];
-                }
-                return sum;
-            }
-
-            //square of a numeber
-            int squareNumber(int number)
-            {
-                return (number * number);
-            }
-
-            //int[] ElevaArrayAlQuadrato(int[] array): che preso un array di numeri interi, restituisca un nuovo array con tutti gli elementi elevati quadrato. Attenzione: è importante restituire un nuovo array, e non modificare l’array come parametro della funzione! Vi ricordate perchè? Pensateci (vedi slide)
-            int[] arrayToSquare(int[] array)
-            {
-                int[] copyArray = (int[])array.Clone();
-                
-                for (int i = 0;i < copyArray.Length;i++)
-                {
-                    int currElem = copyArray[i];
-                    copyArray[i] = squareNumber(currElem);
-                }
-                return copyArray;
-            }
-
+            PrintArray(userArray);
 
         }
+
+
+
+        ////////////////////////////////////////////////
+        /////////////////////FUNCTION///////////////////
+        ////////////////////////////////////////////////
+
+        //print an array in that format [element1, element2, element3, ...]
+        public static void PrintArray(int[] array)
+        {
+            Console.Write("[");
+
+            for (int i = 0; i < array.Length; i++) { 
+                int currentNumber = array[i];
+                if (i < array.Length -1) { 
+                    Console.Write($"{currentNumber}, ");
+                }else
+                {
+                    Console.Write(currentNumber);
+                } 
+
+            }
+
+            Console.Write("]");
+                
+        }
+
+        //sum all element in array
+        public static int ArrayElementSum(int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            return sum;
+        }
+
+        //square of a number
+        public static int SquareNumber(int number)
+        {
+            return (number * number);
+        }
+
+        //do the square of all int number in array
+        public static int[] ArrayToSquare(int[] array)
+        {
+            int[] copyArray = (int[])array.Clone();
+                
+            for (int i = 0;i < copyArray.Length;i++)
+            {
+                int currElem = copyArray[i];
+                copyArray[i] = SquareNumber(currElem);
+            }
+            return copyArray;
+        }
+
     }
 }
